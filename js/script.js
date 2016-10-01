0d0820ce3faf09aa33f03316bf4cd5f66a29ce35
musafirshahid <musafirshahid@gmail.com>
2016-10-01 08:07:45 +0600

    Added About Content

--- js/script.js
+++ js/script.js
@@ -3,5 +3,20 @@ $('.carousel').carousel({
 });
 
 $('.quote-slider').carousel({
-    interval: 10000 //changes the speed
+    interval: 8000 //changes the speed
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
